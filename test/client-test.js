e041d93f2f4fd5d6c70df06c378363b87235d9da
bentaber <ben.taber@gmail.com>
2013-03-10 12:02:25 -0600

    bug: http request callback fires twice on error
    Prevents http connection errors from executing the error
    callback two times, and adds a test.

---
+++ test/client-test.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+    soap = require('..'),
+    assert = require('assert');
+
+module.exports = {
+    'SOAP Client': {
+        'should error on invalid host': function(done) {
+            soap.createClient('http://localhost:1', function(err, client) {
+                assert.ok(err);
+                done();
+            });
+        },        
+    }       
+}
