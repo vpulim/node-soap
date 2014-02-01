f825bf9d0bc0d621844e82f301ca2aa65699712a
Christiaan Westerbeek <chris@devotis.nl>
2014-02-01 22:49:04 +0100

    Fix indention

--- benchmark.js
+++ benchmark.js
@@ -4,24 +4,24 @@ var url = 'test.xml';
 var n = 1000, i = {sax: 0, expat: 0};
 
 var go = function (parser) {
-	if (i[parser] === 0) {
-		console.time(n + ' times using ' + parser);
-	};
+  if (i[parser] === 0) {
+    console.time(n + ' times using ' + parser);
+  }
 
-	soap.createClient(url, {parser: parser}, function(err, client) {
-	    i[parser]++;
-	    if (i[parser] < n) {
-	    	if (i[parser] % 100 === 0) {
-	    		console.log('go ' + parser + ' for the ' + i[parser] + 'th time.');
-	    	}
-	    	setTimeout(function() {
-			    go(parser);
-			}, 0)
-	    }
-	    else {
-	    	console.timeEnd(n + ' times using ' + parser);
-	    }
-	});
+  soap.createClient(url, {parser: parser}, function(err, client) {
+    i[parser]++;
+    if (i[parser] < n) {
+      if (i[parser] % 100 === 0) {
+        console.log('go ' + parser + ' for the ' + i[parser] + 'th time.');
+      }
+      setTimeout(function() {
+        go(parser);
+      }, 0);
+    }
+    else {
+      console.timeEnd(n + ' times using ' + parser);
+    }
+  });
 };
 
 //run two series in parallel
