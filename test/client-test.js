ef509e9d25008157eac6604cbe226bcf776a0b5a
Matt Broadstone <mbroadstone@devonit.com>
2014-03-02 22:18:21 -0500

    jshint is run on tests now, moved some tests over to mocha BDD format

--- test/client-test.js
+++ test/client-test.js
@@ -1,14 +1,14 @@
+"use strict";
+
 var fs = require('fs'),
     soap = require('..'),
     assert = require('assert');
 
-module.exports = {
-    'SOAP Client': {
-        'should error on invalid host': function(done) {
-            soap.createClient('http://localhost:1', function(err, client) {
-                assert.ok(err);
-                done();
-            });
-        },
-    }
-}
+describe('SOAP Client', function() {
+  it('should error on invalid host', function(done) {
+    soap.createClient('http://localhost:1', function(err, client) {
+      assert.ok(err);
+      done();
+    });
+  });
+});
