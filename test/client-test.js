3683084d64d9dccf737e87f01ab47aea3107e023
ankush-garg <agarg.punjabi@gmail.com>
2014-03-07 14:40:36 -0800

    Adding ability to inspect and clear soap headers.

--- test/client-test.js
+++ test/client-test.js
@@ -11,4 +11,22 @@ describe('SOAP Client', function() {
       done();
     });
   });
+  
+  it('should add and clear soap headers', function(done) {
+    soap.createClient(__dirname+'/wsdl/default_namespace.wsdl', function(err, client) {
+      assert.ok(client);
+      assert.ok(!client.getSoapHeaders());
+
+      client.addSoapHeader('header1');
+      client.addSoapHeader('header2');
+
+      assert.ok(client.getSoapHeaders().length === 2);
+      assert.ok(client.getSoapHeaders()[0] === 'header1');
+      assert.ok(client.getSoapHeaders()[1] === 'header2');
+
+      client.clearSoapHeaders();
+      assert.ok(!client.getSoapHeaders());
+      done();
+    });
+  });
 });
