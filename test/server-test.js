72676dcaf7761801dd6fbba5e042207c2c8f2a28
Ghughal, Bakul (CORP) <bakul.ghughal@adp.com>
2015-03-27 16:50:56 -0400

    fixing issue with build failure

--- test/server-test.js
+++ test/server-test.js
@@ -162,11 +162,13 @@ describe('SOAP Server', function() {
   it('should emit \'request\' event', function(done) {
     test.soapServer.on('request', function requestManager(request, methodName) {
       assert.equal(methodName, 'GetLastTradePrice');
-      done();
     });
     soap.createClient(test.baseUrl + '/stockquote?wsdl', function(err, client) {
       assert.ok(!err);
-      client.GetLastTradePrice({ tickerSymbol: 'AAPL'}, function() {});
+      client.GetLastTradePrice({ tickerSymbol: 'AAPL'}, function() {
+        assert.ok(!err);
+        done();
+      });
     });
   });
 
