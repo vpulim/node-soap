5a905b6bf81c4d6504999861c7c33fb7419fe777
Jacob Page <jpage@godaddy.com>
2017-06-09 18:11:00 -0700

    Fix infinite recursion

--- test/client-test.js
+++ test/client-test.js
@@ -854,7 +854,10 @@ var fs = require('fs'),
 
       it('shall generate correct payload for recursively-defined types', function (done) {
         soap.createClient(__dirname + '/wsdl/recursive2.wsdl', function (err, client) {
-          assert.ok(!err);
+          if (err) {
+            return void done(err);
+          }
+
           assert.ok(client);
           client.AddAttribute({
             "Requests":{
@@ -890,8 +893,11 @@ var fs = require('fs'),
                 }
               ]
             }
-          }, function(err) {
-            assert.ok(!err);
+          }, function () {
+            var sentInputContent = client.lastRequest.substring(client.lastRequest.indexOf('<Requests>') + '<Requests>'.length, client.lastRequest.indexOf('</Requests>'));
+            assert.equal(
+              sentInputContent,
+              '<AddAttributeRequest><RequestIdx>1</RequestIdx><Identifier><SystemNamespace>bugrepro</SystemNamespace><ResellerId>1</ResellerId><CustomerNum>860692</CustomerNum><AccountUid>80a6e559-4d65-11e7-bd5b-0050569a12d7</AccountUid></Identifier><Attr><AttributeId>716</AttributeId><IsTemplateAttribute>0</IsTemplateAttribute><ReadOnly>0</ReadOnly><CanBeModified>1</CanBeModified><Name>domain</Name><AccountElements><AccountElement><ElementId>1693</ElementId><Name>domain</Name><Value>foo</Value><ReadOnly>0</ReadOnly><CanBeModified>1</CanBeModified></AccountElement></AccountElements></Attr><RequestedBy>blah</RequestedBy><RequestedByLogin>system</RequestedByLogin></AddAttributeRequest>');
             done();
           });
         });
