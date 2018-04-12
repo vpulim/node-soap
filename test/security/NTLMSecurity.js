e180e0f7c07a52b56aeef7086f77f16eca916014
Ben Hanna <ben.hanna@vpdcs.com>
2018-04-12 10:42:58 -0400

    Added tests for NTLMSecurity.

--- test/security/NTLMSecurity.js
+++ test/security/NTLMSecurity.js
@@ -8,4 +8,23 @@ describe('NTLMSecurity', function() {
   it('is a function', function() {
     NTLMSecurity.should.be.type('function');
   });
+
+  describe('addHeaders', function () {
+    it('should set connection as \'keep-alive\'', function () {
+      var headers = {};
+      var instance = new NTLMSecurity(username, password);
+      instance.addHeaders(headers);
+      headers.should.have.property('Connection', 'keep-alive');
+    });
+  });
+
+  describe('defaultOption param', function() {
+    it('is used in addOptions', function() {
+      var options = {};
+      var instance = new NTLMSecurity(username, password);
+      instance.addOptions(options);
+      options.should.have.property("username", username);
+      options.should.have.property("password", password);
+    });
+  });
 });
