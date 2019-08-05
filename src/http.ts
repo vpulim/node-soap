c61512b52ca783b311cd00db7691d0f60f431631
Adrian Gabrielli <adi.gabrielli@gmail.com>
2019-08-05 16:20:47 +0300

    cleaned code + added test

--- src/http.ts
+++ src/http.ts
@@ -85,20 +85,20 @@ export class HttpClient {
       followAllRedirects: true,
     };
 
-    if (attachments.length > 0) {
+    if (exoptions.alwaysMTOM || attachments.length > 0) {
       const start = uuid();
       let action = null;
-      if (headers['Content-Type'].indexOf("action") > -1) {
-           for (let ct of headers['Content-Type'].split("; ")){
-               if (ct.indexOf("action") > -1){
+      if (headers['Content-Type'].indexOf('action') > -1) {
+           for (const ct of headers['Content-Type'].split('; ')) {
+               if (ct.indexOf('action') > -1) {
                     action = ct;
                }
            }
       }
       headers['Content-Type'] =
         'multipart/related; type="application/xop+xml"; start="<' + start + '>"; start-info="text/xml"; boundary=' + uuid();
-      if (action){
-          headers['Content-Type'] = headers['Content-Type'] + "; " + action;
+      if (action) {
+          headers['Content-Type'] = headers['Content-Type'] + '; ' + action;
       }
       const multipart: any[] = [{
         'Content-Type': 'application/xop+xml; charset=UTF-8; type="text/xml"',
