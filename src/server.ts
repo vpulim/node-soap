4af9a557eeef55ca5942a47e0a5566a5a0e50f53
sigitas-stumbras <37151370+sigitas-stumbras@users.noreply.github.com>
2019-11-13 20:17:45 +0200

    SOAP envelope 1.2 schema valid Fault response
    SOAP envelope 1.2 schema valid Fault response (http://www.w3.org/2003/05/soap-envelope)

--- src/server.ts
+++ src/server.ts
@@ -406,10 +406,17 @@ export class Server extends EventEmitter {
         if (authResult instanceof Error) {
           return this._sendError({
             Code: {
-              Value: 'SOAP-ENV:Server',
-              Subcode: { value: 'InternalServerError' },
+              Value: _this_1.wsdl.options.forceSoap12Headers
+                ? 'soap:Receiver'
+                : 'soap:Server',
+              Subcode: { Value: 'InternalServerError' },
             },
-            Reason: { Text: authResult.toString() },
+            Reason: { 
+              Text: {
+                attributes: { "xml:lang": 'en' },
+                $value: authResult.toString()
+              }                  
+            },
             statusCode: 500,
           }, callback, includeTimestamp);
         }
@@ -425,20 +432,34 @@ export class Server extends EventEmitter {
               }
               return this._sendError({
                 Code: {
-                  Value: 'SOAP-ENV:Server',
-                  Subcode: { value: 'InternalServerError' },
+                  Value: _this_1.wsdl.options.forceSoap12Headers
+                    ? 'soap:Receiver'
+                    : 'soap:Server',
+                  Subcode: { Value: 'InternalServerError' }
                 },
-                Reason: { Text: error.toString() },
+                Reason: {
+                  Text: {
+                    attributes: { "xml:lang": 'en' },
+                    $value: authResult.toString()
+                  }
+                },
                 statusCode: 500,
               }, callback, includeTimestamp);
             }
           } else {
             return this._sendError({
               Code: {
-                Value: 'SOAP-ENV:Client',
+                Value: _this_1.wsdl.options.forceSoap12Headers
+                    ? 'soap:Sender'
+                    : 'soap:Client',
                 Subcode: { value: 'AuthenticationFailure' },
               },
-              Reason: { Text: 'Invalid username or password' },
+              Reason: { 
+                Text: {
+                  attributes: { "xml:lang": 'en' },
+                  $value: 'Invalid username or password'
+                }                  
+              },
               statusCode: 401,
             }, callback, includeTimestamp);
           }
@@ -507,10 +528,17 @@ export class Server extends EventEmitter {
         } else {
           return this._sendError({
             Code: {
-              Value: 'SOAP-ENV:Server',
+              Value: _this_1.wsdl.options.forceSoap12Headers
+                ? 'soap:Receiver'
+                : 'soap:Server',
               Subcode: { value: 'InternalServerError' },
             },
-            Reason: { Text: error.toString() },
+            Reason: {
+              Text: {
+                attributes: { "xml:lang": 'en' },
+                $value: authResult.toString()
+              }
+            },
             statusCode: 500,
           }, callback, includeTimestamp);
         }
