533ff7c1ceb4faa4aa112d9b7eb1079581de2b35
Stephane Tondini <stondini@cbhbank.com>
2021-05-10 22:05:52 +0200

    Adding complex wsdl test "should parse complex wsdls" in file "wsdl-parse-test.js".

--- test/wsdl-parse-test.js
+++ test/wsdl-parse-test.js
@@ -54,4 +54,80 @@ describe(__filename, function () {
       done();
     });
   });
+
+  it('should parse complex wsdls', function (done) {
+    open_wsdl(path.resolve(__dirname, 'wsdl/complex/registration.wsdl'), function (err, def) {
+      if (err) {
+        return done( err );
+      }
+
+      if (null === def.findSchemaType('recipientAddress', 'http://test-soap.com/api/common/types')) {
+        return done( 'Unable to find "recipientAddress" complex type' );
+      }
+      if (null === def.findSchemaType('commonAddress', 'http://test-soap.com/api/common/types')) {
+        return done( 'Unable to find "commonAddress" complex type' );
+      }
+      if (null === def.findSchemaType('companyAddress', 'http://test-soap.com/api/common/types')) {
+        return done( 'Unable to find "companyAddress" complex type' );
+      }
+      if (null === def.findSchemaObject('http://test-soap.com/api/registration/messages', 'registerUserRequest')) {
+        return done( 'Unable to find "registerUserRequest" schema object' );
+      }
+
+      var requestBody = {
+        id: 'FI_ID_VALUE',
+        lastName: 'Doe',
+        firstName: 'John',
+        dateOfBirth: '1970-01-01',
+        correspondenceLanguage: 'ENG',
+        emailAddress: 'jdoe@doe.com',
+        lookupPermission: 'ALLOWED',
+        companyAddress: {
+          address: {
+            streetName: 'Street',
+            postalCode: 'Code',
+            city: 'City',
+            countryCode: 'US'
+          },
+          companyName: 'ACME'
+        }
+      }
+
+      var requestAsXML = def.objectToDocumentXML(
+        'registerUserRequest', 
+        requestBody, 
+        'msg',
+        'http://test-soap.com/api/registration/messages',
+        'registerUserRequest'
+      );
+
+      console.log(requestAsXML);
+
+      /*
+      Expected XML
+      <msg:registerUserRequest xmlns:msg="http://test-soap.com/api/registration/messages" xmlns="http://test-soap.com/api/registration/messages">
+        <msg:id>FI_ID_VALUE</msg:id>
+        <msg:lastName>Doe</msg:lastName>
+        <msg:firstName>John</msg:firstName>
+        <msg:dateOfBirth>1970-01-01</msg:dateOfBirth>
+        <msg:correspondenceLanguage>ENG</msg:correspondenceLanguage>
+        <msg:emailAddress>jdoe@doe.com</msg:emailAddress>
+        <msg:lookupPermission>ALLOWED</msg:lookupPermission>
+        <msg:companyAddress>
+          <ct:address xmlns:ct="http://test-soap.com/api/common/types">
+            <ct:streetName>Street</ct:streetName>
+            <ct:postalCode>Code</ct:postalCode>
+            <ct:city>City</ct:city>
+            <ct:countryCode>US</ct:countryCode>
+          </ct:address>
+          <ct:companyName>ACME</ct:companyName>
+        </msg:companyAddress>
+      </msg:registerUserRequest>
+      */
+      assert.strictEqual(requestAsXML, '<msg:registerUserRequest xmlns:msg="http://test-soap.com/api/registration/messages" xmlns="http://test-soap.com/api/registration/messages"><msg:id>FI_ID_VALUE</msg:id><msg:lastName>Doe</msg:lastName><msg:firstName>John</msg:firstName><msg:dateOfBirth>1970-01-01</msg:dateOfBirth><msg:correspondenceLanguage>ENG</msg:correspondenceLanguage><msg:emailAddress>jdoe@doe.com</msg:emailAddress><msg:lookupPermission>ALLOWED</msg:lookupPermission><msg:companyAddress><ct:address xmlns:ct="http://test-soap.com/api/common/types"><ct:streetName>Street</ct:streetName><ct:postalCode>Code</ct:postalCode><ct:city>City</ct:city><ct:countryCode>US</ct:countryCode></ct:address><ct:companyName>ACME</ct:companyName></msg:companyAddress></msg:registerUserRequest>');
+
+      done();
+    });
+  });
+
 });
