28f473d3b02a128afe9f2ff529f8601392d77db0
vision10 <sorinm025@gmail.com>
2021-09-06 21:36:29 +0300

    minor fix for ntlm request, bump package version

--- src/http.ts
+++ src/http.ts
@@ -85,10 +85,11 @@ export class HttpClient implements IHttpClient {
       url: curl.href,
       method: method,
       headers: headers,
-      validateStatus: null,
       transformResponse: (data) => data,
     };
-
+    if (!exoptions.ntlm) {
+      options.validateStatus = null;
+    }
     if (exoptions.forceMTOM || attachments.length > 0) {
       const start = uuidv4();
       let action = null;
