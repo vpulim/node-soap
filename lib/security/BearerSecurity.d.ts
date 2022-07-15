0f985f63ee2e81012d3ebaac1842d16d3ee65970
Don Nguyen <nguyen.donbui@gmail.com>
2022-07-15 00:50:26 -0500

    Remove lib dir

--- lib/security/BearerSecurity.d.ts
+++
@@ -1,9 +0,0 @@
-import { IHeaders, ISecurity } from '../types';
-export declare class BearerSecurity implements ISecurity {
-    private defaults;
-    private _token;
-    constructor(token: string, defaults?: any);
-    addHeaders(headers: IHeaders): void;
-    toXML(): string;
-    addOptions(options: any): void;
-}
