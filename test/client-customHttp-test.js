88c21d41d5a92329f8f7a4758efa0eef77605fd5
Sorin <sori_vlad@yahoo.com>
2023-02-10 16:38:35 +0200

    updated axios and axios-ntlm to latest

--- test/client-customHttp-test.js
+++ test/client-customHttp-test.js
@@ -94,9 +94,9 @@ it('should allow customization of httpClient and the wsdl file download should p
       });
     }
     //Now write the response with the wsdl
-    var state = httpResStream.write('HTTP/1.1 200 OK\r\nContent-Type: text/xml; charset=utf-8\r\nContent-Length: 1904\r\n\r\n'+wsdl);
+    var state = httpResStream.write('HTTP/1.1 200 OK\r\nContent-Type: text/xml; charset=utf-8\r\nContent-Length: '+wsdl.length+'\r\n\r\n'+wsdl);
   });
-
+  
   var httpCustomClient = new MyHttpClient({}, socketStream);
   var url = 'http://localhost:50000/Platform.asmx?wsdl';
   soap.createClient(url,
