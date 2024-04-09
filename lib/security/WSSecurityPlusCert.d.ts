f73327666a2637a45bddc6a8cb55e16fb338bab5
amit-iwell <amit.kumar@investwellonline.com>
2024-04-09 18:31:42 +0530

    revert the changes

--- lib/security/WSSecurityPlusCert.d.ts
+++
@@ -1,9 +0,0 @@
-import { ISecurity } from '../types';
-import { WSSecurity } from './WSSecurity';
-import { WSSecurityCert } from './WSSecurityCert';
-export declare class WSSecurityPlusCert implements ISecurity {
-    private readonly wsSecurity;
-    private readonly wsSecurityCert;
-    constructor(wsSecurity: WSSecurity, wsSecurityCert: WSSecurityCert);
-    postProcess(xml: string, envelopeKey: string): string;
-}
