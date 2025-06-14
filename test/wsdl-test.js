1754117bd4263efcbe6159a4976365098eb8d207
Serguei Mokhov <mokhov@cse.concordia.ca>
2025-06-14 03:59:39 -0400

    Allows SchemaElement instance to use import namespace as targetNamespace (#1296)
     - Allows SchemaElement instance to use import namespace as targetNamespace when the attribute is not set
     - Prevent to log error message when schema elements do not include targetNamespace attribute:
       `Target-Namespace "undefined" already in use by another Schema!`
     - Added test for undefined targetNamespaces
    
    Co-authored-by: Niche <ndeniche@gmail.com>
    Co-authored-by: Rubén Alberto Reyes <ruben.reyes@ficohsa.com>

--- test/wsdl-test.js
+++ test/wsdl-test.js
@@ -420,4 +420,27 @@ describe('WSDL Parser (non-strict)', () => {
       done();
     });
   });
+
+  it('should return the right amount of schemas with no targetNamespace', (done) => {
+    soap.createClient(__dirname+'/wsdl/schema_with_no_targetnamespace.wsdl', function(err, client) {
+      assert.ifError(err);
+      assert.equal(Object.keys(client.wsdl.definitions.schemas).length, 2);
+      assert.ok(
+        client.wsdl.definitions.schemas.hasOwnProperty('http://www.Dummy.com/Common/Types') &&
+        client.wsdl.definitions.schemas.hasOwnProperty('http://www.Dummy.com/Name/Types')
+      )
+      done();
+    });
+  });
+
+  it('should not return both schemas when targetNamespace is undefined (no imports)', (done) => {
+    soap.createClient(
+      __dirname + "/wsdl/schemas_without_targetnamespace.wsdl",
+      function(err, client) {
+        assert.ifError(err);
+        assert.equal(Object.keys(client.wsdl.definitions.schemas).length, 1);
+        done();
+      }
+    );
+  })
 });
