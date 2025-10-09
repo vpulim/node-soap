37974b7c0a529005261c7eec99dbb19b6c231a80
Vasily Martynov <vasily.martynov@gmail.com>
2025-10-09 19:36:00 +1300

    formayt

--- test/security/WSSecurity.js
+++ test/security/WSSecurity.js
@@ -63,14 +63,14 @@ describe('WSSecurity', function () {
     equal(
       xml,
       `<wsse:Security soap:actor="urn:sample" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">` +
-      `<wsu:Timestamp wsu:Id="Timestamp-2025-10-06T00:00:00Z">` +
-      `<wsu:Created>2025-10-06T00:00:00Z</wsu:Created>` +
-      `<wsu:Expires>2025-10-06T00:10:00Z</wsu:Expires>` +
-      `</wsu:Timestamp><wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="SecurityToken-2025-10-06T00:00:00Z">` +
-      `<wsse:Username>my&amp;User</wsse:Username>` +
-      `<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">my&amp;Pass</wsse:Password>` +
-      `<wsu:Created>2025-10-06T00:00:00Z</wsu:Created>` +
-      `</wsse:UsernameToken></wsse:Security>`,
+        `<wsu:Timestamp wsu:Id="Timestamp-2025-10-06T00:00:00Z">` +
+        `<wsu:Created>2025-10-06T00:00:00Z</wsu:Created>` +
+        `<wsu:Expires>2025-10-06T00:10:00Z</wsu:Expires>` +
+        `</wsu:Timestamp><wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="SecurityToken-2025-10-06T00:00:00Z">` +
+        `<wsse:Username>my&amp;User</wsse:Username>` +
+        `<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">my&amp;Pass</wsse:Password>` +
+        `<wsu:Created>2025-10-06T00:00:00Z</wsu:Created>` +
+        `</wsse:UsernameToken></wsse:Security>`,
     );
   });
 
@@ -102,12 +102,13 @@ describe('WSSecurity', function () {
     equal(
       xml,
       `<wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">` +
-      `<wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="SecurityToken-2025-10-06T00:00:00Z">` +
-      `<wsse:Username>myUser</wsse:Username>` +
-      `<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">myPass</wsse:Password>` +
-      `<wsu:Created>2025-10-06T00:00:00Z</wsu:Created>` +
-      `</wsse:UsernameToken>` +
-      `<custom:MyCustomElement xmlns:custom="http://example.com/custom">foo</custom:MyCustomElement>` +
-      `</wsse:Security>`)
+        `<wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="SecurityToken-2025-10-06T00:00:00Z">` +
+        `<wsse:Username>myUser</wsse:Username>` +
+        `<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">myPass</wsse:Password>` +
+        `<wsu:Created>2025-10-06T00:00:00Z</wsu:Created>` +
+        `</wsse:UsernameToken>` +
+        `<custom:MyCustomElement xmlns:custom="http://example.com/custom">foo</custom:MyCustomElement>` +
+        `</wsse:Security>`,
+    );
   });
 });
