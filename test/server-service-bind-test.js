f0149d6396ef2d8c792860e1a0af7d3778aa5714
Vasily Martynov <vasily.martynov@gmail.com>
2025-10-22 17:56:09 +1300

    fix format

--- test/server-service-bind-test.js
+++ test/server-service-bind-test.js
@@ -14,27 +14,26 @@ describe('SOAP Server Binding Resolution', function () {
       Hello_Port: {
         sayHello: function (args) {
           return {
-            greeting: args.firstName
+            greeting: args.firstName,
           };
-        }
-      }
+        },
+      },
     },
     Hello_Service2: {
       Hello_Port2: {
         sayHello2: function (args) {
           return {
-            greeting: args.firstName
+            greeting: args.firstName,
           };
-        }
-      }
-    }
+        },
+      },
+    },
   };
 
   this.beforeAll(async function () {
-
     server = http.createServer(function (req, res) {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.write("Hello");
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('Hello');
       res.end();
     });
 
@@ -48,7 +47,7 @@ describe('SOAP Server Binding Resolution', function () {
   });
 
   it('should correctly resolve serviceName, portName, and binding', async function () {
-    const client = await soap.createClientAsync(wsdl)
+    const client = await soap.createClientAsync(wsdl);
     assert.ok(client);
     console.log('client created:', client.describe());
 
